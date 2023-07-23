aa4ab69f8ba9fbbfc30b35b8d9b3c14abd1ae46c
kalem868 <kalemmalek123@gmail.com>
2023-07-23 19:07:15 -0400

    hmm

--- app/main.py
+++ app/main.py
@@ -16,9 +16,22 @@ app.register_blueprint(question_solution_routes, url_prefix=api_prefix)
 app.register_blueprint(subject_routes, url_prefix=api_prefix)
 app.register_blueprint(csec_section_routes, url_prefix=api_prefix)
 
+
+
+def get_client_ip():
+    # Check for the X-Forwarded-For header to get the client's public IP address
+    x_forwarded_for = request.headers.get('X-Forwarded-For')
+    if x_forwarded_for:
+        # The X-Forwarded-For header can contain a comma-separated list of IPs.
+        # The client's IP address will be the first one in the list.
+        return x_forwarded_for.split(',')[0]
+    else:
+        # If X-Forwarded-For is not available, use request.remote_addr as a fallback
+        return request.remote_addr
+
 @app.route('/api/getIP', methods=['GET'])
 def get_ip():
-    client_ip = request.remote_addr
+    client_ip = get_client_ip()
     return jsonify(ip=client_ip)
 
 if __name__ == "__main__":
